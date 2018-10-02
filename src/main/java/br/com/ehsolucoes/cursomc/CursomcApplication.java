38002669310a3d827f1c3c22d08883c2ae90edd0
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2018-10-02 17:26:15 -0300

    Produto e associacao muitos para muitos

--- src/main/java/br/com/ehsolucoes/cursomc/CursomcApplication.java
+++ src/main/java/br/com/ehsolucoes/cursomc/CursomcApplication.java
@@ -1,12 +1,46 @@
 package br.com.ehsolucoes.cursomc;
 
+import br.com.ehsolucoes.cursomc.domain.Categoria;
+import br.com.ehsolucoes.cursomc.domain.Produto;
+import br.com.ehsolucoes.cursomc.repositories.CategoriaRepository;
+import br.com.ehsolucoes.cursomc.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
     }
 }
--- src/main/java/br/com/ehsolucoes/cursomc/domain/Produto.java
+++ src/main/java/br/com/ehsolucoes/cursomc/domain/Produto.java
@@ -4,10 +4,9 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
-public class Categoria  implements Serializable {
+public class Produto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -15,17 +14,22 @@ public class Categoria  implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+    private Double preco;
 
-    @ManyToMany(mappedBy = "categorias")
-    private List<Produto> produtos = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(
+            name = "PRODUTO_CATEGORIA",
+            joinColumns = @JoinColumn(name="produto_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private List<Categoria> categorias = new ArrayList<>();
 
-    public Categoria(){
-    }
+    public Produto(){ }
 
-    public Categoria(Integer id, String nome) {
-        super();
+    public Produto(Integer id, String nome, Double preco) {
         this.id = id;
         this.nome = nome;
+        this.preco = preco;
     }
 
     public Integer getId() {
@@ -44,12 +48,20 @@ public class Categoria  implements Serializable {
         this.nome = nome;
     }
 
-    public List<Produto> getProdutos(){
-        return produtos;
+    public Double getPreco() {
+        return preco;
     }
 
-    public void setProdutos(List<Produto> produtos){
-        this.produtos = produtos;
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
     }
 
     @Override
@@ -60,7 +72,7 @@ public class Categoria  implements Serializable {
             return  false;
         if(getClass() != obj.getClass())
             return false;
-        Categoria other = (Categoria)obj;
+        Produto other = (Produto)obj;
         if(id == null){
             if(other.id != null)
                 return false;
