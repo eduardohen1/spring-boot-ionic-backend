d41ca4ae388796bf25fbf00821c45523a1a41cbb
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2018-10-09 16:18:59 -0300

    Inserindo uma nova Categoria com post

--- src/main/java/br/com/ehsolucoes/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/ehsolucoes/cursomc/resources/CategoriaResource.java
@@ -4,11 +4,10 @@ import br.com.ehsolucoes.cursomc.domain.Categoria;
 import br.com.ehsolucoes.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +24,17 @@ public class CategoriaResource {
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok()
                              .body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(obj.getId())
+                    .toUri() ; //pegar a url passada por parametro
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/br/com/ehsolucoes/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/ehsolucoes/cursomc/services/CategoriaService.java
@@ -23,4 +23,9 @@ public class CategoriaService {
         );
     }
 
+    public Categoria insert(Categoria obj){
+        obj.setId(null);
+        return repo.save(obj);
+    }
+
 }
