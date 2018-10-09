3f5fab77c0957de7909b965f5bd929da2545782d
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2018-10-09 16:57:43 -0300

    Listando dotas as Categorias com DTO

--- src/main/java/br/com/ehsolucoes/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/ehsolucoes/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package br.com.ehsolucoes.cursomc.resources;
 
 import br.com.ehsolucoes.cursomc.domain.Categoria;
+import br.com.ehsolucoes.cursomc.dto.CategoriaDTO;
 import br.com.ehsolucoes.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -48,5 +50,13 @@ public class CategoriaResource {
     public ResponseEntity<Void> delete(@PathVariable Integer id){
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok()
+                .body(listDto);
     }
 }
--- src/main/java/br/com/ehsolucoes/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/ehsolucoes/cursomc/services/CategoriaService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -42,6 +43,10 @@ public class CategoriaService {
         }catch (DataIntegrityViolationException e){
             throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
         }
+    }
+
+    public List<Categoria> findAll(){
+        return repo.findAll();
     }
 
 }
