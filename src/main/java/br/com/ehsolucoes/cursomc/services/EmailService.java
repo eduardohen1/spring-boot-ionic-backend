0bbd0fc0e32280c3f1d085bc1689de8e3af7bc61
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2018-11-13 00:40:22 -0200

    Email HTML

--- src/main/java/br/com/ehsolucoes/cursomc/services/EmailService.java
+++ src/main/java/br/com/ehsolucoes/cursomc/services/EmailService.java
@@ -3,10 +3,16 @@ package br.com.ehsolucoes.cursomc.services;
 import br.com.ehsolucoes.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 
 }
--- src/main/java/br/com/ehsolucoes/cursomc/services/SmtpEmailService.java
+++ src/main/java/br/com/ehsolucoes/cursomc/services/SmtpEmailService.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -13,11 +16,22 @@ public class SmtpEmailService extends AbstractEmailService {
     @Autowired
     private MailSender mailSender;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @Override
     public void sendEmail(SimpleMailMessage msg) {
         LOG.info("Enviando e-mail...");
         //erro ao enviar.. caixa de saida - estudar mais
         //mailSender.send(msg);
+        LOG.info("E-mail enviado!");
+    }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        LOG.info("Enviando e-mail HTML...");
+        //erro ao enviar.. caixa de saida - estudar mais
+        javaMailSender.send(msg);
         LOG.info("E-mail enviado!");
     }
 }
