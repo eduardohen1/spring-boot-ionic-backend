ad2996c94a46d5e898a254558d7554fd7d7b045e
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2018-12-09 22:22:47 -0200

    Refresh token

---
+++ src/main/java/br/com/ehsolucoes/cursomc/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package br.com.ehsolucoes.cursomc.resources;
+
+import br.com.ehsolucoes.cursomc.security.JWTUtil;
+import br.com.ehsolucoes.cursomc.security.UserSS;
+import br.com.ehsolucoes.cursomc.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value="/refresh_token", method=RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authentiated();
+        String token = jwtUtil.genereteToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+}
