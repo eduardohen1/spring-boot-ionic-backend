4695b9a9ea45289764e4d55b9c9f567db92e63b0
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2018-12-10 00:40:22 -0200

    Salvando primeiro arquivo no s3

--- src/main/java/br/com/ehsolucoes/cursomc/CursomcApplication.java
+++ src/main/java/br/com/ehsolucoes/cursomc/CursomcApplication.java
@@ -4,6 +4,7 @@ import br.com.ehsolucoes.cursomc.domain.*;
 import br.com.ehsolucoes.cursomc.domain.enums.EstadoPagamento;
 import br.com.ehsolucoes.cursomc.domain.enums.TipoCliente;
 import br.com.ehsolucoes.cursomc.repositories.*;
+import br.com.ehsolucoes.cursomc.services.S3Service;
 import org.hibernate.annotations.NaturalId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -17,13 +18,16 @@ import java.util.Arrays;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+    @Autowired
+    private S3Service s3Service;
+
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        s3Service.uploadFile("G:\\Desenvolvimento\\Java\\cursomc\\teste.jpg");
     }
 }
 
